0d7aeada6fcc5874043cc81c4b6665eac891b479
Geunseok Byeon <byeongeunseok@gmail.com>
2023-06-13 00:15:02 +0900

    Creating a web page using Flask

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 # TODO: Error Handling
 # TODO: More Website
 
+from flask import Flask, render_template, request
 from extractors.wwr import extract_wwr_jobs
 from file import save_to_file
 
-keyword = input("What do you want to search for? -> ")
+# Create a flask app
+app = Flask("WebScrap")
 
-wwr = extract_wwr_jobs(keyword)
+# Initialize
+@app.route("/")
+def init():
+    return render_template("main.html")
 
-jobs = wwr  # + wwr + wwr
+# Search page
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    wwr = extract_wwr_jobs(keyword)
+    jobs = wwr  # + wwr + wwr
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
-save_to_file(keyword, jobs)
+
+# Run flask app
+app.run("localhost")
